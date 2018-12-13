a0ecc810f58dbdebc2fd5f637661c1bbf508604f
flatfisher <shimano.entou@gmail.com>
2018-12-13 22:30:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
+const kind = 'Feed';
+const projectId = process.env.GOOGLE_CLOUD_PROJECT;
 
 // Start server
 const bodyParser = require('body-parser')
@@ -15,7 +17,26 @@ app.listen(PORT, () => {
     console.log('Started');
 });
 
+// Preparing Cloud Datastore
+const Datastore = require('@google-cloud/datastore');
+const datastore = new Datastore({
+    projectId: projectId,
+});
+
 // Handlers
 app.get('/', (_, res) => {
     res.status(200).send('Hello, world!').end();
 });
+
+// Functions
+
+// Cloud Datastore
+// Read Feed url
+async function addFeedTask() {
+    const query = datastore.createQuery(kind)
+    const results = await datastore.runQuery(query)
+    const feeds = results[0]
+    feeds.forEach(feed => {
+        console.log(feed)
+    })
+}
